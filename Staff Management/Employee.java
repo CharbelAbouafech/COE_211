82c6310cca1a3781de0e2cb3c71c01c3c097952c
CharbelAbouafech <100039298+CharbelAbouafech@users.noreply.github.com>
2022-02-21 18:23:06 +0200

    Add files via upload

---
+++ Staff Management/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+   	private String First_Name;
+	private String Last_Name;
+	private int age;
+	private double Monthly_Salary;
+
+
+    public void Employee(String FirstName, String LastName, int Age, double MonthlySalary) {
+	this.First_Name = FirstName;
+	this.Last_Name = LastName;
+	this.age = Age;
+	this.Monthly_Salary = MonthlySalary;
+
+    }
+}
+
